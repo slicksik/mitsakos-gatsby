868184db219c495c19100fb28d1aa46369c2fd89
slicksik <vtopouz@cyclopsweb.gr>
2019-12-12 17:56:01 +0200

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emma - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Τυπογραφείο Mητσάκος`,
   },
   plugins: [
     {
